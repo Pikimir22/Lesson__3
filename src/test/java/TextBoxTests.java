4d1d440a2208877e894abeb0ee0aced76ad0f6d4
Pikimir22 <egor.nikitin22@yandex.ru>
2022-11-15 21:21:07 +0300

    v.4

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -23,90 +24,70 @@ public class TextBoxTests {
         executeJavaScript("$('#fixedban').remove()");
     }
 
+@Test
+    void fillFormTest(){
 
-    @Test
-    void Name() {
+        //name
         String firstName = "First";
         String lastName = "Test";
 
         $x(("//input[@id='firstName']")).setValue("First");
         $x(("//input[@id='lastName']")).setValue("Test");
 
-        $x(("//input[@id='firstName']")).shouldHave(value(firstName));
-        $(By.xpath("//input[@id='lastName']")).shouldHave(value(lastName));
-    }
-
-    @Test
-    void Email() {
+        //email
         String email = "testEmail@yandex.ru";
 
         $x(("//input[@id='userEmail']")).setValue("testEmail@yandex.ru");
 
-        $x("//input[@id='userEmail']").shouldHave(value(email));
+        //gender
 
-    }
-
-    @Test
-    void Gender() {
         $(".custom-control-label").click();
-    }
 
-
-    @Test
-    void Mobile() {
+        //mobile
         $x("//input[@placeholder=\"Mobile Number\"]").setValue("1234567890");
-        $x(("//input[@placeholder=\"Mobile Number\"]")).shouldHave(value("1234567890"));
-    }
 
-    @Test
-    void DateOfBirth() {
-        $x("//init put[@id=\"dateOfBirthInput\"]").click();
+        //dateOfBirth
+        $x("//input[@id=\"dateOfBirthInput\"]").click();
         $x("//div[text()=\"18\"]").click();
 
-    }
-
-    @Test
-    void Subjects() {
+        //subjects
         $x("//input[@id=\"subjectsInput\"]").click();
         $x("//input[@id=\"subjectsInput\"]").sendKeys("Maths");
         $x("//input[@id=\"subjectsInput\"]").sendKeys(Keys.ENTER);
 
-        $(By.xpath("//textarea[@id=\"currentAddress\"]")).shouldHave(text("Maths"));
+        //currenAddress
 
-    }
-
-    @Test
-    void CurrentAddress() {
         $x("//textarea[@id=\"currentAddress\"]").click();
         $x("//textarea[@id=\"currentAddress\"]").setValue("Apt 42");
 
-        $(By.xpath("//textarea[@id=\"currentAddress\"]")).shouldHave(text("Apt 42"));
+        //stateAndCity
 
-    }
-
-
-    @Test
-    void StateAndCity() {
         String state = "Uttar Pradesh";
         String city = "Lucknow";
 
-        //State
-        $(".css-tlfecz-indicatorContainer").click();
-        $x("//div[text()='Uttar Pradesh']").click();
-        $(By.xpath("//div[text()='Uttar Pradesh']")).shouldHave(text(state));
+        //state
+       // $x("//input[@id=\"react-select-3-input\"]").click();
+        //$x("//div[text()='Uttar Pradesh']").click();
 
+        //city
+    $("#state").click();
+    $("#stateCity-wrapper").$(new ByText("Uttar Pradesh")).click();
+    $("#city").click();
+    $("#stateCity-wrapper").$(new ByText("Agra")).click();
 
-//City
-        $(".css-tlfecz-indicatorContainer").click();
-        $x("//div[text()='Lucknow']").click();
-        $(By.xpath("//div[text()='Lucknow']")).shouldHave(text(city));
+//        $(".css-tlfecz-indicatorContainer").click();
+//        $x("//div[text()='Lucknow']").click();
 
-
-    }
-
-    @AfterAll
-    static void Submit() {
+        //submit
         $x("//button[@id=\"submit\"]").click();
+
+
+    $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+    $(".table-responsive").shouldHave(text("First Test"), text(email), text("18 November,2022"), text("Male"), text("Maths"), text("Apt 42"));
     }
+
+
+
+
 
 }
