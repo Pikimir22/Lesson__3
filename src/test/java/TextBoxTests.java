c32d4752ec4e45857482a6ace049f557d9d950ac
Pikimir22 <egor.nikitin22@yandex.ru>
2022-11-16 21:53:33 +0300

    v.3

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selenide.*;
@@ -24,70 +26,50 @@ public class TextBoxTests {
         executeJavaScript("$('#fixedban').remove()");
     }
 
-@Test
-    void fillFormTest(){
+    @Test
+    void fillFormTest() {
 
         //name
-        String firstName = "First";
-        String lastName = "Test";
-
+        String firstTest = "First Test";
         $x(("//input[@id='firstName']")).setValue("First");
         $x(("//input[@id='lastName']")).setValue("Test");
-
         //email
         String email = "testEmail@yandex.ru";
-
         $x(("//input[@id='userEmail']")).setValue("testEmail@yandex.ru");
-
         //gender
-
         $(".custom-control-label").click();
-
         //mobile
         $x("//input[@placeholder=\"Mobile Number\"]").setValue("1234567890");
-
         //dateOfBirth
-        $x("//input[@id=\"dateOfBirthInput\"]").click();
-        $x("//div[text()=\"18\"]").click();
-
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("August");
+        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__day--008:not(.react-datepicker__day--outside-month)").click();
         //subjects
         $x("//input[@id=\"subjectsInput\"]").click();
         $x("//input[@id=\"subjectsInput\"]").sendKeys("Maths");
         $x("//input[@id=\"subjectsInput\"]").sendKeys(Keys.ENTER);
-
+        //image
+        $("#hobbiesWrapper").$(new ByText("Reading")).click();
+        $("#uploadPicture").uploadFile(new File("C:\\Users\\UserMSI\\Desktop\\forTest.jpg"));
         //currenAddress
-
         $x("//textarea[@id=\"currentAddress\"]").click();
         $x("//textarea[@id=\"currentAddress\"]").setValue("Apt 42");
-
         //stateAndCity
-
         String state = "Uttar Pradesh";
         String city = "Lucknow";
-
         //state
-       // $x("//input[@id=\"react-select-3-input\"]").click();
-        //$x("//div[text()='Uttar Pradesh']").click();
-
+        $("#state").click();
+        $("#stateCity-wrapper").$(new ByText("Uttar Pradesh")).click();
         //city
-    $("#state").click();
-    $("#stateCity-wrapper").$(new ByText("Uttar Pradesh")).click();
-    $("#city").click();
-    $("#stateCity-wrapper").$(new ByText("Agra")).click();
-
-//        $(".css-tlfecz-indicatorContainer").click();
-//        $x("//div[text()='Lucknow']").click();
-
+        $("#city").click();
+        $("#stateCity-wrapper").$(new ByText("Lucknow")).click();
         //submit
         $x("//button[@id=\"submit\"]").click();
 
-
-    $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-    $(".table-responsive").shouldHave(text("First Test"), text(email), text("18 November,2022"), text("Male"), text("Maths"), text("Apt 42"));
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text(firstTest), text(email), text("08 August,1994"), text("Male"), text("Maths"), text("Apt 42"),text(state),text(city));
     }
-
-
-
 
 
 }
