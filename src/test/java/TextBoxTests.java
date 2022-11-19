9d95679f25c80227cfa52599eacdad1e175062d9
Pikimir22 <egor.nikitin22@yandex.ru>
2022-11-19 17:09:10 +0300

    v/3

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -8,8 +8,7 @@ import org.openqa.selenium.Keys;
 
 import java.io.File;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -28,16 +27,17 @@ public class TextBoxTests {
 
     @Test
     void fillFormTest() {
-        String firstTest = "First Test";
+        String firstName = "First";
+        String lastName = "Test";
         String email = "testEmail@yandex.ru";
         String state = "Uttar Pradesh";
         String city = "Lucknow";
         String mobileNumber = "1234567890";
         //name
-        $x(("//input[@id='firstName']")).setValue("First");
-        $x(("//input[@id='lastName']")).setValue("Test");
+        $(("#firstName")).setValue(firstName);
+        $(("#lastName")).setValue(lastName);
         //email
-        $x(("//input[@id='userEmail']")).setValue("testEmail@yandex.ru");
+        $(("#userEmail")).setValue(email);
         //gender
         $x("//label[text()='Male']").click();
         //mobile
@@ -48,9 +48,9 @@ public class TextBoxTests {
         $(".react-datepicker__year-select").selectOption("1994");
         $(".react-datepicker__day--008:not(.react-datepicker__day--outside-month)").click();
         //subjects
-        $x("//input[@id=\"subjectsInput\"]").click();
-        $x("//input[@id=\"subjectsInput\"]").sendKeys("Maths");
-        $x("//input[@id=\"subjectsInput\"]").sendKeys(Keys.ENTER);
+        $("#subjectsInput").click();
+        $x("//input[@id=\"subjectsInput\"]").setValue("Maths").pressEnter();
+
         //Hobbies
         $("#hobbiesWrapper").$(new ByText("Reading")).click();
         //image
@@ -67,8 +67,10 @@ public class TextBoxTests {
         //submit
         $x("//button[@id=\"submit\"]").click();
 
+        $(".modal-dialog").should(appear);
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(firstTest), text(email), text("Male"), text(mobileNumber), text("08 August,1994"), text("Maths"),text("Reading"), text("Apt 42"), text(state), text(city));
+
+        $(".table-responsive").shouldHave(text(firstName), text(lastName), text(email), text("Male"), text(mobileNumber), text("08 August,1994"), text("Maths"), text("Reading"), text("Apt 42"), text(state), text(city));
     }
 
 
